d134c7df1c999c1f521fb2ce9c3bb8e43e290156
Agryo Araujo <agryostallion@gmail.com>
2024-12-21 15:03:03 -0300

    Iniciando código de carregamento.

--- js/boot.js
+++ js/boot.js
@@ -1,3 +1,22 @@
+/* Métodos fundamentais do Phaser
+   1- preload(); = Responsável pelo pre-carregamento de alguma coisa.
+   2- create();  = Responsável pela configuração, criação de um State.
+   3- update();  = Responsável pelo processamento da lógica do Jogo.
+   4- render();  = Usado para fazer ma verificação do código "debug" do Jogo.
+*/
 var bootState = {
-    
+    // Como está trabalhando com Objeto, primeiro diz o nome da função "preload:" e diz que é uma função.
+    // Item do objeto "preload" dois pontos e informa que é uma função "funciton(){}"
+    preload: function(){
+        // O "game" que é o código que foi instanciado com esse nome, "load" carrega, "image" uma imagem.
+        // Dentro da função passa o ID (nome) na imagem e o local dela entre aspas simples.
+        game.load.image('progressBar','img/progressBar.png');
+    },
+    // A virgula é obrigatória pois abaixo já é outro item no Objeto, o item "create".
+
+    // O "create" vai ser chamado logo após serem carregados todos os arquivos acima no "preload".
+    create: function(){
+        // O "game" agora chama o State e inicia (start) o "load"
+        game.state.start('load');
+    }
 };
--- js/load.js
+++ js/load.js
@@ -1,1 +1,22 @@
-var loadState = {};
+var loadState = {
+    preload: function(){
+        /* Esse "preload" cria a variável da imagem instanciada anteriormente.
+           A variável "progressBar" recebe do "game" adicionando "add" um Sprite do jogo que é a imagem
+           já carregada no "load".
+           Nos parenteses coloca as coordenadas onde a imagem irá ser mostrada, X e Y.
+           O "game.world.centerX" faz o calculo do Eixo X para ficar no centro.
+           O Eixo Y fica a 250px.
+           E por fim qual a imagem foi carregada lá no "load".
+        */
+        var progressBar = game.add.sprite(game.world.centerX,250,'progressBar');
+            /* Aqui está definindo o ponto de ancoragem da imagem no centro dela mesma.
+               Eixo X = 0.5 e Eixo y = 0.5 como ambos são no centro, basta colocar apenas ".5".
+               "anchor" .set(.5) é igual a .set(0.5,0.5)
+            */
+            progressBar.anchor.set(.5);
+            /* Aqui está informando que a imagem será usada como "Barra de Progresso" o percentual de carregamento.
+               O "game"  carrega "load" o Sprite de progresso "setPreloadSprite" usando a imagem "progressBar".
+            */
+            game.load.setPreloadSprite(progressBar);
+    }
+};
