e27ceabc9f5f3d8311937a5515cd0376baebd0db
Agryo Araujo <agryostallion@gmail.com>
2024-12-21 12:32:17 -0300

    Arquivos usados no projeto.

--- js/game.js
+++ js/game.js
@@ -1,6 +1,9 @@
 // Variável "game" recebe um "new" Phaser.Game com as dimensões 750px por 500px e renderização "CANVAS" do Phaser
 var game = new Phaser.Game(750,500,Phaser.CANVAS);
 
+    // Aqui estou instanciando todos os códigos que vão ser usados.
+    // O "game", a variável que já foi criada, "state.add" adicionando o State com o nome entre aspas simples e o
+    // código que foi criado na pasta JS. O nome que será usado no restante do código é esse entre aspas.
     game.state.add('boot',bootState);
     game.state.add('load',loadState);
     game.state.add('menu',menuState);
@@ -8,4 +11,6 @@ var game = new Phaser.Game(750,500,Phaser.CANVAS);
     game.state.add('stage2',stage2State);
     game.state.add('end',endState);
 
+    // Aqui ele inicia o jogo pelo "boot" como já foi falado que agora usa-se o nome que foi instanciado.
+    // O "game" inicia o State "boot".
     game.state.start('boot');
