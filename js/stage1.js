0ab328fe2e1addc9396d2d51c6c55305fdc1adba
Agryo Araujo <agryostallion@gmail.com>
2024-12-23 23:22:19 -0300

    Adiciona os eventos de fim de jogo.

--- js/stage1.js
+++ js/stage1.js
@@ -1,5 +1,8 @@
 var stage1State = {
   create: function () {
+    // Cria a variável que informa se o jogo inicia ou para (acabou).
+    this.onGame = true;
+
     /*
       Iniciar a música do jogo.
       Variável Global "music" do "manuState" usando o "this" para isso.
@@ -147,7 +150,11 @@ var stage1State = {
       Entre os parenteses os parametros, Eixo X, Eixo Y, Texto concatenado a variável "this.coins" e estilo da fonte.
     */
     this.coins = 0;
-    this.txtCoins = game.add.text(15, 15, 'MOEDAS: ' + this.getText(this.coins), {font: '15px emulogic', fill: '#fff'});
+    this.txtCoins = game.add.text(15, 
+      15, 
+      'MOEDAS: ' + this.getText(this.coins), 
+      {font: '15px emulogic', fill: '#fff'}
+    );
 
     /*
       Contador da pontuação
@@ -157,8 +164,9 @@ var stage1State = {
       game.world.centerX, 
       15, 
       'PONTOS: ' + this.getText(game.global.score), 
-      {font:'15px emulogic', fill:'#fff'});
-      this.txtPontos.anchor.set(.5, 0);
+      {font:'15px emulogic', fill:'#fff'}
+    );
+    this.txtPontos.anchor.set(.5, 0);
 
     /*
       Controles Movimentação
@@ -180,25 +188,127 @@ var stage1State = {
     this.emitter.setYSpeed(-50, 50);
     // E configura para elas não serem afetadas pela gravidade.
     this.emitter.gravity.y = 0;
+
+    /*
+      Criação do tempo, temporizador do jogo
+      Primeiro cria a variável que armazena o tempo.
+      Depois cria a variável que mostra o texto do tempo no jogo com suas configurações nos parametros.
+      E centraliza no final da tela.
+    */
+    this.tempo = 5;
+    this.txtTempo = game.add.text(
+      game.world.width - 15, 
+      15, 
+      'TEMPO: ' + this.getText(this.tempo), 
+      {font:'15px emulogic', fill:"#fff"}
+    );
+    this.txtTempo.anchor.set(1, 0);
+    // Cria a variável "timer" que recebe um loop repetido ".loop()" que é um evento ".events" de Tempo ".time" no Jogo "game".
+    // Parametros -> ( ): Tempo para repetir "1000ms", a Função pra chamar a cada repetição e o contexto "this".
+    this.timer = game.time.events.loop(1000, function(){
+      // Diminui o tempo em 1 segundo
+      this.tempo--;
+      // Atualiza o tempo visualmente no jogo.
+      this.txtTempo.text = 'TEMPO: ' + this.getText(this.tempo);
+    }, this);
   },
 
   update: function () {
-    // Agora informa que os blocos e o jogador podem colidir entre si.
-    game.physics.arcade.collide(this.player, this.blocks);
-    /* 
-      Agora informa que o jogador pode coletar as moedas. O "overlap" verifica se estão ocupando o mesmo espaço.
-      Entre os parenteses os parametros são:
-      Objeto1 a ser verificado se estão no mesmo espaço, Objeto2 da  verificação, Função para executar, Condição "null".
-      Não vai ter condição para disparar a função então usa "null" e o "this" é o contexto da função.
-    */
-    game.physics.arcade.overlap(this.player, this.coin, this.getCoin, null, this);
-    // Informa que o Inimigo pode ter contato com o jogador para roubar moedas.
-    game.physics.arcade.overlap(this.player, this.inimigo, this.loseCoin, null, this);
+    // Condição para iniciar o jogo, SE a variável "this.onGame" for iniciar "true", então o jogo é iniciado.
+    if (this.onGame) {
+      // Agora informa que os blocos e o jogador podem colidir entre si.
+      game.physics.arcade.collide(this.player, this.blocks);
+      /* 
+        Agora informa que o jogador pode coletar as moedas. O "overlap" verifica se estão ocupando o mesmo espaço.
+        Entre os parenteses os parametros são:
+        Objeto1 a ser verificado se estão no mesmo espaço, Objeto2 da  verificação, Função para executar, Condição "null".
+        Não vai ter condição para disparar a função então usa "null" e o "this" é o contexto da função.
+      */
+      game.physics.arcade.overlap(this.player, this.coin, this.getCoin, null, this);
+      // Informa que o Inimigo pode ter contato com o jogador para roubar moedas.
+      game.physics.arcade.overlap(this.player, this.inimigo, this.loseCoin, null, this);
 
-    // Inicia a função de movimentação do Inimigo.
-    this.moveInimigo();
-    // Inicia a função de movimentação do Jogador.
-    this.movePlayer();
+      // Inicia a função de movimentação do Inimigo.
+      this.moveInimigo();
+      // Inicia a função de movimentação do Jogador.
+      this.movePlayer();
+
+      /*
+        Condição do fim de jogo.
+        SE o tempo acabar OU o jogador atigir o objetivo de coletar "10" moedas nesse caso.
+        Assim ela executa as operações nescessárias para finalizar o jogo.
+      */
+      if (this.tempo < 1 || this.coins >= 10) {
+        // Chama a Função de Fim de Jogo.
+        this.gameOver();
+
+        // Para o tempo removendo o evento criado acima.
+        // Remova o Temporizador ".remove(this.timer)" do Evento ".events" de Tempo ".time" do Jogo "game".
+        game.time.events.remove(this.timer);
+
+        // Agora para o personagem no Eixo X e Eixo Y.
+        this.player.body.velocity.x = 0;
+        this.player.body.velocity.y = 0;
+        // Para a animação do personagem.
+        this.player.animations.stop();
+        // E para o personagem na primeira imagem que ele está virado para a tela do jogador.
+        this.player.frame = 0;
+
+        // Para o Inimigo também. O inimigo já para nois eixos pois a função dele está dentro do "Update".
+        this.inimigo.animations.stop();
+        this.inimigo.frame = 0;
+
+        /*
+          Condição para testar se o jogo acabou por causa do tempo ou passou de fase.
+          SE o jogador pegar a quantidade de moedas suficientes, entra no primeiro caso.
+          SE NÂO o jogador não conseguiu pegar as moedas no tempo do jogo.
+        */
+        if (this.coins >= 10) {
+          
+        } else {
+          // Exibe a mensagem de Fim de Jogo.
+          const txtGameOver = game.add.text(game.world.centerX, 250, 'GAME OVER', {font:'30px emulogic', fill:'#fff'});
+          txtGameOver.anchor.set(.5);
+        }
+
+        // Aqui exibe a melhor pontuação independente se passou de fase ou se perdeu.
+        const txtBestScore = this.game.add.text(
+          game.world.centerX, 
+          350, 
+          'MELHOR RESULTADO: ' + this.getText(game.global.record), 
+          {font:'20px emulogic', fill:"#fff"}
+        );
+        txtBestScore.anchor.set(.5);
+
+        /*
+          Agora cria o evento que chama novamente o Menu Principal
+          Primeiro cria um evento que após um tempo chama a função.
+          Parametros: Tempo de duração para chamar o evento "5000ms", a Função que executa e o contexto "this".
+        */
+        game.time.events.add(5000, function() {
+          // Para a musica do Stage atual.
+          this.music.stop();
+          // Verifica se passou de fase ou se acabou o tempo.
+          if (this.coins >= 10) {
+            // SE atingiu as moedas suficientes, passa de fase.
+
+          } else {
+            // SE NÃO chama o "Menu"
+            game.state.start('menu');
+          }
+        }, this);
+      }
+    }
+  },
+
+  /*
+    Função Fim de Jogo (Game Over)
+    Primeiro finaliza o Jogo.
+    
+  */
+  gameOver: function () {
+    // Quando o jogo acaba, por tempo ou conclusão, o jogo acaba. A variável "this.onGame" recebe "false".
+    this.onGame = false;
   },
 
   /*
